f809655d3845e5d8a02418ceb3c0a94dba7108f6
vika <chararchter@pm.me>
2023-06-14 16:00:03 +0300

    shorter

--- translation-example.py
+++ translation-example.py
@@ -1,28 +1,28 @@
-import json
-
 import requests
 from transformers import pipeline
 
 from secret_api_token import API_TOKEN
 
+text_to_translate = "kad ir nākamais vilciens"
+
 # use local model
 # git clone https://huggingface.co/Helsinki-NLP/opus-mt-tc-big-lv-en
-text_to_translate = "kad ir nākamais vilciens"
 
 pipe = pipeline("translation", model=".\opus-mt-tc-big-lv-en")
 translated_text = pipe(text_to_translate)
 
 print(translated_text)
 
+
 # use API
-API_URL_LV = "https://api-inference.huggingface.co/models/Helsinki-NLP/opus-mt-tc-big-lv-en"
-
-headers = {"Authorization": f"Bearer {API_TOKEN}"}
-def query(payload: str, api_url: str) -> dict:
-    data = json.dumps(payload)
-    response = requests.request("POST", api_url, headers=headers, data=data)
-    return json.loads(response.content.decode("utf-8"))
+def query(payload, model_id, api_token):
+    headers = {"Authorization": f"Bearer {api_token}"}
+    api_url = f"https://api-inference.huggingface.co/models/{model_id}"
+    response = requests.post(api_url, headers=headers, json=payload)
+    return response.json()
 
 
-translated_text = query(text_to_translate, API_URL_LV)
+model_id = "Helsinki-NLP/opus-mt-tc-big-lv-en"
+
+translated_text = query(text_to_translate, model_id, API_TOKEN)
 print(translated_text)
